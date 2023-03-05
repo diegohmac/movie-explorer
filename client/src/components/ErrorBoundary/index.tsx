9ba07dcaa5cd444d031fb8b95eb5a97726971b81
Diego Machado <machadodiegoh@gmail.com>
2023-03-05 16:47:23 -0300

    feat: added Error Boundary

---
+++ client/src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,39 @@
+import { Component, ErrorInfo, ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+interface State {
+  hasError: boolean
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  }
+
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true }
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Uncaught error:', error, errorInfo)
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return (
+        <>
+          <h1>Sorry...</h1>
+          <p>Something went wrong.</p>
+          <button onClick={() => location.reload()}>Try again</button>
+        </>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
